d024c63ace066b3482a6501cfeb21a2d70e4ce1f
srinu <srinurao1902@gmail.com>
2026-08-03 12:13:29 +0530

    revert: point login buttons back to internal /login/ route

---
+++ revert_login.py
@@ -0,0 +1,13 @@
+﻿import re
+
+html_path = 'c:/Users/ASUS/Downloads/rekonnect1/rekonnect/core/templates/core/index.html'
+with open(html_path, 'r', encoding='utf-8') as f:
+    html_content = f.read()
+
+html_content = html_content.replace(
+    'href="https://konnect-iota.vercel.app/" target="_blank" rel="noopener noreferrer"',
+    'href="{% url \'core:login\' %}"'
+)
+
+with open(html_path, 'w', encoding='utf-8') as f:
+    f.write(html_content)
