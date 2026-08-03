58d978871f68c5f45633a536d542fef127b76218
srinu <srinurao1902@gmail.com>
2026-08-03 12:08:10 +0530

    feat: redirect login buttons to external vercel app

---
+++ update_login.py
@@ -0,0 +1,11 @@
+﻿html_path = 'c:/Users/ASUS/Downloads/rekonnect1/rekonnect/core/templates/core/index.html'
+with open(html_path, 'r', encoding='utf-8') as f:
+    html_content = f.read()
+
+html_content = html_content.replace(
+    'href="{% url \'core:login\' %}"',
+    'href="https://konnect-iota.vercel.app/" target="_blank" rel="noopener noreferrer"'
+)
+
+with open(html_path, 'w', encoding='utf-8') as f:
+    f.write(html_content)
